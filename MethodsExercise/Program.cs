8dfd689f8733adb2e76880c0f2f08be6d2429f0a
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-30 20:40:55 -0500

    start here

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //build a mad lib here with user input
+
+
         }
     }
 }
