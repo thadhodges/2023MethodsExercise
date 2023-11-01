a828a863006f2e32aef3cc22d1c2fdee888e21d6
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-31 19:24:04 -0500

    Mad Lib

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,8 +4,37 @@
     {
         static void Main(string[] args)
         {
-            //build a mad lib here with user input
+            Console.WriteLine("Welcome to the C# Mad Libs game!");
 
+            // Prompt the user for various inputs
+            Console.Write("Enter an adjective: ");
+            string? adjective1 = Console.ReadLine();
+
+            Console.Write("Enter a noun: ");
+            string? noun1 = Console.ReadLine();
+
+            Console.Write("Enter a verb (past tense): ");
+            string? verb1 = Console.ReadLine();
+
+            Console.Write("Enter an adverb: ");
+            string? adverb = Console.ReadLine();
+
+            Console.Write("Enter a noun (plural): ");
+            string? pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a verb (present tense): ");
+            string? verb2 = Console.ReadLine();
+
+            // Construct the Mad Libs story
+            string madLibsStory = $"Today, I saw a {adjective1} {noun1} that {verb1} {adverb} " +
+                                  $"across the park. Later, it joined a group of {pluralNoun} " +
+                                  $"and they all started to {verb2} together.";
+
+            // Display the resulting story
+            Console.WriteLine("\nHere's your Mad Libs story:");
+            Console.WriteLine(madLibsStory);
+
+            Console.ReadLine(); // Wait for user to press Enter to exit
 
         }
     }
