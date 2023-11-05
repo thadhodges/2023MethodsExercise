0261f7be0a1368b8acaeff77fe4880f504fdd445
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-11-05 08:57:00 -0600

    Created a Methods class to build out more methods

---
+++ MethodsExercise/TestMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class TestMethods
+    {
+        //Method to determine if sum of numbers is even or odd...
+        static void SumIsEven()
+        {
+
+
+
+        }
+
+
+        //Method to determine if product of a set of numbers is even or odd
+
+
+
+    }
+}
